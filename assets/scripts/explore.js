733bab726c780b18348f90c3c71c985a1b28d1f3
Ruiping <irisfang123@gmail.com>
2024-05-05 13:32:32 -0700

    update explore.js

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,40 @@
 // explore.js
 
-window.addEventListener('DOMContentLoaded', init);
+window.addEventListener("DOMContentLoaded", init);
 
 function init() {
   // TODO
+  const voiceSelect = document.querySelector("#voice-select");
+  const textarea = document.getElementById("text-to-speak");
+  const button = document.querySelector("button");
+  const img = document.querySelector("img");
+
+  const synth = window.speechSynthesis;
+  let voices = synth.getVoices(); // return a list of supported voices
+
+  // map Voice objects to html option elements
+  for (let i = 0; i < voices.length; i++) {
+    const option = document.createElement("option");
+    option.textContent = `${voices[i].name} (${voices[i].lang})`;
+    option.setAttribute("value", voices[i].name);
+    voiceSelect.appendChild(option);
+  }
+
+  button.addEventListener("click", (event) => {
+    let utterance = new SpeechSynthesisUtterance(textarea.value);
+    utterance.addEventListener("end", () => {
+      img.src = "assets/images/smiling.png";
+    });
+    const selectedOption = voiceSelect.value;
+    // map selected option back to Voice object
+    for (let i = 0; i < voices.length; i++) {
+      let voice = voices[i];
+      if (selectedOption == voice.name) {
+        utterance.voice = voice;
+        break;
+      }
+    }
+    img.src = "assets/images/smiling-open.png";
+    synth.speak(utterance);
+  });
 }
